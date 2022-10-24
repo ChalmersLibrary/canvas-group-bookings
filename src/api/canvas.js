0e5dd6b7b518aab13f870a35159abe5453ca7ae4
Rolf Johansson <rolf.johansson@chalmers.se>
2022-10-24 19:11:08 +0200

    Added function for creating conversation in canvas.

--- src/api/canvas.js
+++ src/api/canvas.js
@@ -62,6 +62,86 @@ async function addCacheWrite(cacheName) {
     })
 }
 
+/**
+ * @description Post a message in Conversations (Inbox) for specific group(s)
+ * @param {number} recipients List of recipients according to API
+ * @param {string} subject Message subject
+ * @param {string} body Message body
+ * @param {number} userid Calling user id
+ */
+async function createConversation(recipients, subject, body, userId) {
+    try {
+        let thisApiPath = API_HOST + API_PATH + "/conversations";
+        let apiData;
+        let errorCount = 0;
+
+        await auth.findAccessToken(userId).then(async (token) => {
+            while (errorCount < 2 && thisApiPath && token) {
+                log.info("POST " + thisApiPath);
+
+                try {
+                    const response = await axios.post(thisApiPath, {
+                        headers: {
+                            "User-Agent": "Chalmers/Azure/Request",
+                            "Authorization": token.token_type + " " + token.access_token
+                        },
+                        "recipients[]": recipients.join(","),
+                        "group_conversation": true,
+                        "subject": subject,
+                        "body": body
+                    });
+        
+                    apiData = response.data;
+        
+                    if (response.headers["X-Request-Cost"]) {
+                        log.info("Request cost: " + response.headers["X-Request-Cost"]);
+                    }
+        
+                    thisApiPath = false;
+                }
+                catch (error) {
+                    errorCount++;
+                
+                    console.error(error);
+
+                    if (error.response.status == 401 && error.response.headers['www-authenticate']) { // refresh token, then try again
+                        log.info("401, with www-authenticate header.");
+
+                        await auth.refreshAccessToken(userId).then((result) => {
+                            if (result.success) {
+                                log.info("Refreshed access token.");
+                            }
+                            else {
+                                log.info(result);
+                            }
+                        });
+                    }
+                    else if (error.response.status == 401 && !error.response.headers['www-authenticate']) { // no access, redirect to auth
+                        log.error("Not authorized in Canvas for use of this API endpoint.");
+                        return(error);
+                    }
+                    else {
+                        log.error(error);
+                        return(error);
+                    }
+                }
+            }    
+        }).catch((error) => {
+            log.error(error);
+            return (error);
+        });
+
+        return new Promise((resolve) => {
+            resolve(apiData);
+        });
+    }
+    catch (error) {
+        return new Promise((reject) => {
+            reject(error);
+        }); 
+    }
+}
+
 /* Get details for a specified user in course */
 async function getUserDetails(courseId, userId, user_id) {
     try {
@@ -426,5 +506,6 @@ async function getCourseGroups(courseId, userId) {
 module.exports = {
     getUserDetails,
     getGroupDetails,
-    getCourseGroups
+    getCourseGroups,
+    createConversation
 }
