d7be4f02e52bd8d96f545e11beb87e66f7c6806f
Rolf Johansson <rojo@nocrew.org>
2022-11-10 16:32:35 +0100

    Use replaceAll() as variables can be used more than once.

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -30,17 +30,17 @@ function capitalizeFirstLetter(string) {
 }
 
 function replaceMessageMagics(body, course_name, reservation_message, cancellation_policy_hours, user_name, time_start, location_name, location_url, instructor_name, instructor_email, group_name, group_names) {
-    body = body.replace("{{reservation_course_name}}", course_name);
-    body = body.replace("{{reservation_message}}", reservation_message);
-    body = body.replace("{{reservation_group_name}}", group_name);
-    body = body.replace("{{slot_group_names}}", group_names);
-    body = body.replace("{{cancellation_policy_hours}}", cancellation_policy_hours);
-    body = body.replace("{{canvas_user_name}}", user_name);
-    body = body.replace("{{reservation_slot_time}}", time_start);
-    body = body.replace("{{location_name}}", location_name + (location_url ? ", " + location_url : ""));
-    body = body.replace("{{instructor_name}}", instructor_name);
-    body = body.replace("{{instructor_email}}", instructor_email);
-    body = body.replace("{{CONVERSATION_ROBOT_NAME}}", process.env.CONVERSATION_ROBOT_NAME ? process.env.CONVERSATION_ROBOT_NAME : "Canvas Conversation Robot");
+    body = body.replaceAll("{{reservation_course_name}}", course_name);
+    body = body.replaceAll("{{reservation_message}}", reservation_message);
+    body = body.replaceAll("{{reservation_group_name}}", group_name);
+    body = body.replaceAll("{{slot_group_names}}", group_names);
+    body = body.replaceAll("{{cancellation_policy_hours}}", cancellation_policy_hours);
+    body = body.replaceAll("{{canvas_user_name}}", user_name);
+    body = body.replaceAll("{{reservation_slot_time}}", time_start);
+    body = body.replaceAll("{{location_name}}", location_name + (location_url ? ", " + location_url : ""));
+    body = body.replaceAll("{{instructor_name}}", instructor_name);
+    body = body.replaceAll("{{instructor_email}}", instructor_email);
+    body = body.replaceAll("{{CONVERSATION_ROBOT_NAME}}", process.env.CONVERSATION_ROBOT_NAME ? process.env.CONVERSATION_ROBOT_NAME : "Canvas Conversation Robot");
 
     return body;
 }
