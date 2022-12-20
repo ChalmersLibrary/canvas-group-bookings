3837c8c2487a6a4df040bf65dfa037c93b5b76c4
Rolf Johansson <rolf.johansson@chalmers.se>
2022-12-20 20:49:46 +0100

    Read environment from local .env file, something goes wrong otherwise.

--- src/test/load-test-start.sh
+++ src/test/load-test-start.sh
@@ -3,20 +3,20 @@
 # Start parallel load testers for more traffic, loop is just time
 
 echo "Starting load tester for user01"
-time ./load-test.sh user01 100 &
+time ./load-test.sh user01 500 &
 sleep 2
 
 echo "Starting load tester for user02"
-time ./load-test.sh user02 100 &
+time ./load-test.sh user02 500 &
 sleep 2
 
-# 1500 ~ 70m
+# 1500 ~ 70m, 100 ~ 5m
 echo "Starting load tester for user03"
-time ./load-test.sh user03 100 &
+time ./load-test.sh user03 500 &
 sleep 2
 
 echo "Starting load tester for user03"
-time ./load-test.sh user04 100 &
+time ./load-test.sh user04 500 &
 
 echo "Starter is done."
 exit
