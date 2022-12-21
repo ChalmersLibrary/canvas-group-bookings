5ea9c0b25c4d590794387530006f73aaf8d3ac47
Rolf Johansson <rojo@nocrew.org>
2022-12-21 14:40:49 +0100

    Add daily rotate of logs for winston.

--- src/logging/index.js
+++ src/logging/index.js
@@ -1,24 +1,20 @@
 'use strict';
 
 const winston = require('winston');
+const { combine, timestamp, json, errors } = winston.format;
+require('winston-daily-rotate-file');
+
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+    filename: './logs/combined-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    maxFiles: '14d',
+});
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        winston.format.json(),
-    ),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
-      new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
-      new winston.transports.File({ filename: './logs/combined.log' }),
-    ]
+    format: combine(errors({ stack: true }), timestamp(), json()),
+    defaultMeta: {},
+    transports: [fileRotateTransport]
 });
 
 if (process.env.NODE_ENV !== 'production') {
