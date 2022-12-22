3ad34079f00cf23bfa6724fde46cc7861be91e8b
Rolf Johansson <rojo@nocrew.org>
2022-12-22 13:20:17 +0100

    Hotfix for sync issues in db development/main.

--- src/db/setup_3.sql
+++ src/db/setup_3.sql
@@ -1,6 +1,6 @@
 -- Add max_groups and max_indivuduals to location to be able to override course max numbers.
 
-alter table location add column max_groups integer;
-alter table location add column max_individuals integer;
+-- alter table location add column max_groups integer;
+-- alter table location add column max_individuals integer;
 
 insert into version(db_version) values (3);
